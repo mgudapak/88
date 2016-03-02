8b917da61958fc8662b2045b72fb542a6e8b2d55
Mahesh Gudapakkam <mahesh.gudapakkam@gmail.com>
2016-03-02 10:41:04 -0500

    cleaned up how app is wired up and starts up

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+#!/usr/local/bin/python
+
 # File that is invoked to start up a development server.
 # It gets a copy of the app from your package and runs it.
-
 from app import app
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
