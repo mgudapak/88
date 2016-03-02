b1eed0fafab98452d6df4f820b9c3d638b20a47c
Mahesh Gudapakkam <mahesh.gudapakkam@gmail.com>
2016-03-02 16:22:07 -0500

    successfully processed constellation data in template

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template
 from data.constellations import constellations
 
 constellations = constellations
-# constellation_data_files = constellation_data_files
 
 print constellations
-# print constellation_data_files
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')  # reads from config.py in root folder
