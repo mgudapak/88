8d3065610f74a27bb8a3cf0ce7973a7ae8c46bf0
Mahesh Gudapakkam <mahesh.gudapakkam@gmail.com>
2016-03-02 10:41:46 -0500

    added some structural files for application

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 # File contains most of the configuration variables that your app needs.
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
-CONFIG_VAR = "root"
-RANDOM_VAR = "apple"
+
+
+# Enable protection against *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "secret"
+
+# Secret key for signing cookies
+SECRET_KEY = "secret"
