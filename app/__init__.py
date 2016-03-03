7c7c598c3c4a063667a7ff719dfcacca14bcb433
Mahesh Gudapakkam <mahesh.gudapakkam@gmail.com>
2016-03-03 15:03:37 -0500

    added new route and template to display individual constellations

--- app/__init__.py
+++ app/__init__.py
@@ -12,3 +12,9 @@ app.config.from_object('config')  # reads from config.py in root folder
 @app.route('/constellations')
 def home_page():
     return render_template('index.html', constellations=constellations)
+
+
+@app.route('/constellations/<string:constellation_identifier>')
+def individual_constellation(constellation_identifier):
+    if constellation_identifier in constellations:
+        return render_template('constellation.html', constellation=constellations[constellation_identifier])
